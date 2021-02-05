ea68a88086d597747109725af2e1d58bf9ee8ec1
Undral Batsukh <ubatsukh@esri.com>
2021-02-05 12:15:27 -0800

    more updates

--- devsummit2021/csvLayer-nps/app/main.js
+++ devsummit2021/csvLayer-nps/app/main.js
@@ -311,37 +311,45 @@ define(["require", "exports", "esri/layers/CSVLayer", "esri/layers/FeatureLayer"
     function animateNParks(view) {
         view.whenLayerView(csvLayer).then((layerView) => {
             timeSlider.fullTimeExtent = new TimeExtent({
-                start: new Date(1873, 11, 31),
+                start: new Date(1872, 11, 31),
                 end: new Date(2017, 11, 31)
+            });
+            watchUtils.whenFalseOnce(layerView, "updating", function () {
+                setLayerEffect(layerView);
             });
             timeSlider.watch("timeExtent", function () {
                 csvLayer.definitionExpression = "date_est <= " + timeSlider.timeExtent.end.getTime();
-                var start = new Date(timeSlider.timeExtent.end.getTime());
-                start.setMonth(0);
-                start.setDate(1);
-                layerView.effect = new FeatureEffect({
-                    // excludedLabelsVisible: true,
-                    filter: new FeatureFilter({
-                        timeExtent: new TimeExtent({
-                            start: start,
-                            end: timeSlider.timeExtent.end
-                        })
-                    }),
-                    excludedEffect: "grayscale(80%) blur(2px) opacity(0.3)",
-                    includedEffect: "bloom(1.2, 2px, 0.2)"
-                });
-                const query = layerView.effect.filter.createQuery();
-                query.orderByFields = ["date_est"];
-                let list = "";
-                csvLayer.queryFeatures(query).then(function (results) {
-                    if (results.features.length > 0) {
-                        results.features.forEach(function (feature) {
-                            list = list + `<br/><a href="${feature.attributes.metadata}>${feature.attributes.unit_name}</a>`;
-                        });
-                        document.getElementById("parksDiv").innerHTML = list;
-                    }
-                });
+                setLayerEffect(layerView);
             });
+        });
+    }
+    function setLayerEffect(layerView) {
+        const start = new Date(timeSlider.timeExtent.end.getTime());
+        start.setMonth(0);
+        start.setDate(1);
+        const timeExtent = new TimeExtent({
+            start: start,
+            end: timeSlider.timeExtent.end
+        });
+        layerView.effect = new FeatureEffect({
+            // excludedLabelsVisible: true,
+            filter: new FeatureFilter({
+                timeExtent
+            }),
+            excludedEffect: "grayscale(80%) blur(2px) opacity(0.3)",
+            includedEffect: "bloom(1.2, 2px, 0.2)"
+        });
+        const query = layerView.effect.filter.createQuery();
+        query.orderByFields = ["date_est"];
+        let list = "";
+        csvLayer.queryFeatures(query).then(function (results) {
+            if (results.features.length > 0) {
+                results.features.forEach(function (feature) {
+                    list = list + `<a href="${feature.attributes.metadata}">${feature.attributes.unit_name}</a><br/>`;
+                });
+                document.getElementById("parksDiv").innerHTML = list;
+                console.log(list, "html", document.getElementById("parksDiv").innerHTML);
+            }
         });
     }
 });
